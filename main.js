43335178afaec7bdfe2787a12965234d349bf8a3
Anurag Kumar <47923173+anuragabcr@users.noreply.github.com>
2024-07-08 15:04:23 +0530

    completed

--- main.js
+++ main.js
@@ -1,8 +1,17 @@
 const http = require('http');
+const fs = require('fs');
 
-// TODO: Create an HTTP server
+// Create an HTTP server
+const server = http.createServer((req, res) => {
+    // Get the file path from the command line arguments
+    const filePath = process.argv[2];
 
-// TODO: Listen on port 3000
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end("Hello, World!\n");
+});
 
-// DO NOT EDIT BELOW THIS LINE (Uncomment it once you are done with your code)
-
+// Start the server and listen on port 3000
+const PORT = 3000;
+server.listen(PORT, () => {
+    console.log(`Server is listening on port ${PORT}`);
+});
